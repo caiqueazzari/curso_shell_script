5c252f15a764d0e6f99d38be247b5a2d3e4e57d1
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-22 18:39:17 -0300

    Update ex01.sh

--- ex01.sh
+++ ex01.sh
@@ -1,4 +1,6 @@
 #Write a shell script that prints "Olá, Mundo!" to the screen.
+#Hint 1: Remember to make the shell script executable with the chmod command.
+#Hint 2: Remember to start your script with a shebang!
 
 #!/bin/bash
 
