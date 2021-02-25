dc00299bd8a5f86a8d6c3b050c689474e99039e0
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-25 20:30:58 -0300

    Create ex08.sh

---
+++ ex08.sh
@@ -0,0 +1,18 @@
+#Modify the previous script to accept an unlimited number of files and directories as arguments.
+
+#!/bin/bash
+
+for FILE in $@
+do
+  if [ -f "$FILE" ]
+  then
+    echo "$FILE is a regular file."
+  elif [ -d "$FILE" ]
+  then
+    echo "$FILE is a directory."
+  else
+    echo "$FILE is something other than a regular file or directory."
+  fi
+
+  ls -l $FILE
+done
