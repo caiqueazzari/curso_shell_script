6a8612e9be941e3975d9b47f9771aae9bdda1dc5
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-04-26 16:33:17 -0300

    moved to the folder shell-scripting*

---
+++ shell-scripting-in-a-nutshell/ex006.sh
@@ -0,0 +1,24 @@
+#Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or other type of file. Also perform an ls command against the file or directory with the long listing option.
+
+#!/bin/bash
+
+read -p "Digite o nome de um arquivo ou de um diretório: " FILE
+
+if [ -e "$FILE" ]
+then
+        echo "$FILE existe!"
+else
+        echo "$FILE não existe ou o caminho não foi especificado."
+fi
+
+if [ -f "$FILE" ]
+then
+        echo "E é um arquivo normal!"
+elif [ -d "$FILE" ]
+then
+        echo "E é um diretório!"
+else
+	echo "Não é um arquivo e nem um diretório."
+fi
+
+ls -l $FILE
